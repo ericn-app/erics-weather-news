1acd5408231ac6fa2336701c9d2c4bbf7cf60382
Eric N <ericntd@gmail.com>
2021-12-31 17:09:33 -0500

    Added Bugsnag and Rollbar

--- app/src/main/java/app/ericn/ericsweather/TheApplication.kt
+++ app/src/main/java/app/ericn/ericsweather/TheApplication.kt
@@ -1,6 +1,8 @@
 package app.ericn.ericsweather
 
 import android.app.Application
+import com.bugsnag.android.Bugsnag
+import com.rollbar.android.Rollbar
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -13,6 +15,10 @@ class TheApplication : Application(), HasAndroidInjector {
     override fun onCreate() {
         super.onCreate()
         DaggerAppComponent.factory().create(this).inject(this)
+
+        Bugsnag.start(this)
+
+        Rollbar.init(this);
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
